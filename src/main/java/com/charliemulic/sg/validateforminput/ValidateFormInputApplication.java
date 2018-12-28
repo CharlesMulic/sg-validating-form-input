a07d98fff28f8f431ca640b57eff7aa975cdab5f
Charlie <charles.mulic@gmail.com>
2018-12-28 10:48:45 -0600

    initial

---
+++ src/main/java/com/charliemulic/sg/validateforminput/ValidateFormInputApplication.java
@@ -0,0 +1,14 @@
+package com.charliemulic.sg.validateforminput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateFormInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidateFormInputApplication.class, args);
+    }
+
+}
+
